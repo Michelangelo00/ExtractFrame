f023dd0dc8b64ab712ffd42e970742398aa1fbeb
miche <michelangelo.cellulosi@gmail.com>
2022-09-29 18:47:10 +0200

    Creazione script bash che permette la creazione di file CSV per ogni frame

--- main.py
+++ main.py
@@ -4,15 +4,35 @@
 
 
 import cv2
+import os
 
 webcam = cv2.VideoCapture(0)
 currentframe = 0
+pathExtraction = r"D:\Università\Tirocinio\OpenFace_2.2.0_win_x64\FeatureExtraction.exe"
+command = " -aus -f"
+pathImmagine = r" D:\Università\Tirocinio\Python\ExtractFrame"
+pathComplete = r""
+
+
+#pathComplete = pathExtraction + command + pathImmagine
+#print(pathComplete)
+#os.system(pathComplete)
+
+
 
 while True:
     success, frame = webcam.read()
-
     cv2.imshow("Output", frame)
+    immagine = r"\frame" + str(currentframe) + ".jpg"
     cv2.imwrite('frame' + str(currentframe) + '.jpg', frame)
+    pathComplete = pathExtraction + command + pathImmagine + immagine
+    print(pathComplete)
+    os.system(pathComplete)
+    """
+        TODO:
+            - Estrarre da ogni frame le AU (Provare script bash: FeatureExtraction.exe -aus -f "D:\\Università\\Tirocinio\\Python\\ExtractFrame\\frame32.jpg")
+            - Convertire file CSV in output in file JSON
+    """
     currentframe += 1
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -20,3 +40,4 @@ while True:
 
 webcam.release()
 cv2.destroyAllWindows()
+
